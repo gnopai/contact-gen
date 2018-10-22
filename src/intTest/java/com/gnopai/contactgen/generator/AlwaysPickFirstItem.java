3e497d8454a50ecb8cd1ad618aa333fd727037a9
bennettm <mark.bennett@flightglobal.com>
2018-10-22 08:37:40 -0700

    Added an integration test that runs through generation of a full contact using mock statistics.

---
+++ src/intTest/java/com/gnopai/contactgen/generator/AlwaysPickFirstItem.java
@@ -0,0 +1,41 @@
+package com.gnopai.contactgen.generator;
+
+import com.gnopai.contactgen.generator.random.Chance;
+import com.gnopai.contactgen.generator.random.RandomGenerator;
+import com.gnopai.contactgen.statistics.util.Range;
+import com.gnopai.contactgen.statistics.util.WeightedList;
+
+import java.util.List;
+
+public class AlwaysPickFirstItem implements RandomGenerator {
+
+    @Override
+    public <T> T selectNormallyDistributedListItem(List<T> items) {
+        return items.get(0);
+    }
+
+    @Override
+    public <T> T selectWeightDistributedListItem(WeightedList<T> items) {
+        return items.get(0);
+    }
+
+    @Override
+    public <T> T selectUniformlyDistributedListItem(List<T> items) {
+        return items.get(0);
+    }
+
+    @Override
+    public int selectInteger(int low, int high) {
+        return low;
+    }
+
+    @Override
+    public int selectInteger(Range range) {
+        return range.getLow();
+    }
+
+    @Override
+    public boolean selectChance(Chance chance) {
+        return true;
+    }
+}
